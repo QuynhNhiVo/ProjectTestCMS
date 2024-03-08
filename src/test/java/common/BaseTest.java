4f066d1031ad53903ffe0357d55cbb77ede4b1d1
QuynhNhi <quynhnhi927@gmail.com>
2024-03-08 20:14:26 +0700

    Update Run Multi Browser

--- src/test/java/common/BaseTest.java
+++ src/test/java/common/BaseTest.java
@@ -15,14 +15,38 @@ public class BaseTest {
     @BeforeMethod
     @Parameters({"browser"})
     public void createBrowser(@Optional("chrome") String browserName){
-        if (browserName.equals("chrome")){
-            driver = new ChromeDriver();
-        }if (browserName.equals("firefox")){
-            driver = new FirefoxDriver();
-        }if (browserName.equals("edge")) {
-            driver = new EdgeDriver();
+        setupDriver(browserName);
+    }
+
+    public static WebDriver setupDriver(String browserName){
+        switch (browserName.trim().toLowerCase()){
+            case "chrome":
+                driver = initChromeDriver();
+                break;
+            case "edge":
+                driver = initEdgeDriver();
+                break;
+            default:
+                System.out.println("Browser: " + browserName + " is invalid, Launching Chrome as browser of choice...");
+                driver = initChromeDriver();
         }
+        return driver;
+    }
+
+    private static WebDriver initChromeDriver() {
+        WebDriver driver; //Khai báo Driver cục bộ
+        System.out.println("Launching Chrome browser...");
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
+        return driver;
+    }
+
+    private static WebDriver initEdgeDriver() {
+        WebDriver driver; //Khai báo Driver cục bộ (tạm thời)
+        System.out.println("Launching Edge browser...");
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        return driver;
     }
 
     @AfterMethod
